be10f9fcf033fd54b2d6f00b2aafbe9b72b7f30f
LJS <155962022+SoLeafy@users.noreply.github.com>
2024-04-05 08:55:27 +0900

    vue-router (그저 필기)
    vue-router 폴더는 vue-router 설지 오류로 안됨 (그저 필기)

---
+++ vue-router/src/views/BoardList.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>board입니다</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BoardList'
+}
+</script>
+<style scoped>
+
+</style>
---
+++ vue-router/src/views/LoginPage.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>login입니다</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LoginPage'
+}
+</script>
+<style scoped>
+
+</style>
